66c8c9385a8e380630ee696933c645df8374ef7d
EmirhanEge1 <99147165+EmirhanEge1@users.noreply.github.com>
2022-06-06 00:17:21 +0300

    Update [id].js

--- pages/product-detail/[id].js
+++ pages/product-detail/[id].js
@@ -1,6 +1,5 @@
 
 import { useRouter } from 'next/router';
-/*import Product_details from "../../src/components/product_details" ;*/
 export default function Home() {
 
     const router = useRouter();
